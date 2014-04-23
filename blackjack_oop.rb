7236ed5d4d796383def6bb1b9308b7d724215c9c
Russ Robbins <russ.robbins@gmail.com>
2014-04-23 16:06:49 -0400

    First OOP code

--- blackjack_oop.rb
+++ blackjack_oop.rb
@@ -85,8 +85,8 @@ end
 
 class Player < Participant
   def hit_or_stay deck
-    puts "=> Do you want to (h)it or (s)tay?"
-    cmd = gets.chomp.downcase
+    cmd = prompt "=> Do you want to (h)it or (s)tay?  "
+    cmd.downcase!
     case cmd
     when 'h'
       new_card = deck.deal_card(self)
@@ -97,39 +97,109 @@ class Player < Participant
       puts "Try again..."
       hit_or_stay deck
     end
+    cmd
   end
 end
 
 class Dealer < Participant
   def hit_or_stay deck
     val = hand_value
+    cmd = ''
     case val
     when 1..16
       new_card = deck.deal_card(self)
       puts "#{name} hits and is dealt the #{new_card}."
+      cmd = 'h'
+    when 22..1000
+      puts "#{name} busted!"
+      cmd = 'b'
     else
       puts "#{name} stays."
+      cmd = 's'
     end
+    cmd
   end
 end
 
-# TEST CODE
-new_deck = Deck.new
-player = Player.new("Russ")
-dealer = Dealer.new("Sammy")
-2.times do
-  new_deck.deal_card(player)
-  new_deck.deal_card(dealer)
+def prompt (str)
+  print(str)
+  gets.chomp
 end
 
-puts "#{player}"
-puts player.print_value
-player.hit_or_stay new_deck
-puts "#{player}"
-puts player.print_value
-puts ''
-puts "#{dealer}"
-puts dealer.print_value
-dealer.hit_or_stay new_deck
-puts "#{dealer}"
-puts dealer.print_value
+class Blackjack
+  attr_accessor :deck, :player, :dealer
+
+  def initialize
+    @deck = Deck.new
+    @player = Player.new("Player")
+    @dealer = Dealer.new("Dealer")
+  end
+
+  def get_player_name
+    player.name = prompt "=> What's your name? "
+    player.name.capitalize!
+  end
+
+  def deal_cards
+    puts "Shuffling and dealing cards..."
+    2.times do
+      deck.deal_card(player)
+      deck.deal_card(dealer)
+    end
+  end
+
+  def player_turn
+    puts "#{player.name}'s turn..."
+    puts "#{player}"
+    puts player.print_value
+    cmd = ''
+    until cmd == 's'
+      cmd = player.hit_or_stay deck
+      puts player.print_value
+    end
+  end
+
+  def dealer_turn
+    puts "#{dealer.name}'s turn..."
+    puts "#{dealer}"
+    puts dealer.print_value
+    cmd = ''
+    until cmd == 's' && cmd != 'b'
+      cmd = dealer.hit_or_stay deck
+      puts dealer.print_value
+    end
+  end
+
+  def play
+    get_player_name
+    deal_cards
+    player_turn
+    dealer_turn
+  end
+
+end
+
+game = Blackjack.new
+game.play
+
+
+# TEST CODE
+# new_deck = Deck.new
+# player = Player.new("Russ")
+# dealer = Dealer.new("Sammy")
+# 2.times do
+#   new_deck.deal_card(player)
+#   new_deck.deal_card(dealer)
+# end
+
+# puts "#{player}"
+# puts player.print_value
+# player.hit_or_stay new_deck
+# puts "#{player}"
+# puts player.print_value
+# puts ''
+# puts "#{dealer}"
+# puts dealer.print_value
+# dealer.hit_or_stay new_deck
+# puts "#{dealer}"
+# puts dealer.print_value
